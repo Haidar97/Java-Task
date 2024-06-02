0e39218ec7db39d614882c1e6915b78d0b0e30e8
Haidar97 <haidor.ch@gmail.com>
2024-06-02 22:32:09 +0200

    Update HealthEndpointExtension.java

--- basic/basic-02-health-endpoint/src/main/java/org/eclipse/edc/extension/health/HealthEndpointExtension.java
+++ basic/basic-02-health-endpoint/src/main/java/org/eclipse/edc/extension/health/HealthEndpointExtension.java
@@ -1,31 +1,19 @@
-/*
- *  Copyright (c) 2021 Microsoft Corporation
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Microsoft Corporation - Initial implementation
- *
- */
-
 package org.eclipse.edc.extension.health;
 
-import org.eclipse.edc.runtime.metamodel.annotation.Inject;
-import org.eclipse.edc.spi.system.ServiceExtension;
-import org.eclipse.edc.spi.system.ServiceExtensionContext;
-import org.eclipse.edc.web.spi.WebService;
+package com.example.health;
 
-public class HealthEndpointExtension implements ServiceExtension {
+import org.eclipse.edc.connector.api.management.ManagementApiExtension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.system.configuration.Config;
+
+public class HealthEndpointExtension implements ManagementApiExtension {
 
     @Inject
-    WebService webService;
+    private Config config;
 
     @Override
-    public void initialize(ServiceExtensionContext context) {
-        webService.registerResource(new HealthApiController(context.getMonitor()));
+    public void initialize() {
+         webService.registerResource(new HealthApiController(context.getMonitor()));
     }
 }
+
